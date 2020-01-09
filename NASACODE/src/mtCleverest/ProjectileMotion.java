484b873bc44b331eb81c90eabfc0b4ae2cbb385d
SkylerGentner <57457186+SkylerGentner@users.noreply.github.com>
2020-01-09 12:46:33 -0500

    Fixed Projectile Motion Errors
    Fixed the duplicate and time errors which ultimately shortened the class

--- NASACODE/src/mtCleverest/ProjectileMotion.java
+++ NASACODE/src/mtCleverest/ProjectileMotion.java
@@ -8,7 +8,6 @@ public class ProjectileMotion {
 		
 	}
 	
-	
 	/**
 	 * @param MOON_GRAVITY
 	 * @param xVelocity
@@ -22,10 +21,9 @@ public class ProjectileMotion {
 			ArrayList<Double> yVelocity, ArrayList<Double> angleOfFall,
 			ArrayList<Double> distanceTraveled, ArrayList<Double> height, 
 			ArrayList<Double> time, int index) {
-
 		yVelocity.add(yVelocity.get(index - 1) + MOON_GRAVITY * (time.get(index) - 
 				time.get(index - 1)));
-		xVelocity.add(xVelocity.get(index - 1));
+		xVelocity.add(xVelocity.get(index-1));
 		angleOfFall.add(calcAngle(xVelocity, yVelocity, index));
 		height.add(calcDistance(yVelocity, height, time, index));
 		distanceTraveled.add(calcDistance(xVelocity, distanceTraveled, time, index));
@@ -46,11 +44,8 @@ public class ProjectileMotion {
 			ArrayList<Double> yVelocity, ArrayList<Double> angleOfFall,
 			ArrayList<Double> distanceTraveled, ArrayList<Double> height, ArrayList<Double> time,
 			int index, int timeIndex) {
-		
-		yVelocity.add(yVelocity.get(index - 1) + MOON_GRAVITY * (time.get(timeIndex) - 
-				time.get(index)));
-		xVelocity.add(xVelocity.get(index - 1));
-		angleOfFall.add(calcAngle(xVelocity, yVelocity, index));
+		xVelocity.add(xVelocity.get(index-1));
+		angleOfFall.add(Math.toDegrees(Math.atan((yVelocity.get(index + 1) / xVelocity.get(index)))));
 		height.add(calcDistance(yVelocity, height, time, index, timeIndex));
 		distanceTraveled.add(calcDistance(xVelocity, distanceTraveled, time, index, timeIndex));
 	}
@@ -83,33 +78,12 @@ public class ProjectileMotion {
 	 * @param distanceTraveled
 	 * @param time
 	 * @param index
-	 * @param timeIndex
+	 * @param finalIndex
 	 * @return THE DISTANCE FOR WHEN THE POD HITS THE GROUND
 	 */
-	protected Double calcDistance(ArrayList<Double> velocity, ArrayList<Double> distanceTraveled,
-			ArrayList<Double> time, int index, int timeIndex) {
-		return (.5 * (velocity.get(index) + velocity.get(index - 1)) * (time.get(timeIndex) - 
-				time.get(index))) + distanceTraveled.get(index - 1);
-	}
-
-	/**
-	 * @param yVelocity
-	 * @param index
-	 * @param MOON_GRAVITY
-	 * @return TIME FOR ZERO HEIGHT
-	 */
-	protected double calcTimeAtZeroHeight(ArrayList<Double> yVelocity, ArrayList<Double> time, 
-			int index, final double MOON_GRAVITY) {
-		return (((roundedVelocity(yVelocity, index) - roundedVelocity(yVelocity, index - 1)) /
-				MOON_GRAVITY) + (time.get(index)));
-	}
-	
-	/**
-	 * @param yVelocity
-	 * @param index
-	 * @return velocity rounded to nearest hundredth place
-	 */
-	protected double roundedVelocity(ArrayList<Double> yVelocity, int index) {
-		return Math.round(yVelocity.get(index) * 100) / 100.0;
+	protected double calcDistance(ArrayList<Double> velocity, ArrayList<Double> distanceTraveled,
+			ArrayList<Double> time, int index, int finalIndex) {
+		return (.5 * (velocity.get(finalIndex) + velocity.get(index)) * (time.get(finalIndex) - 
+				time.get(index))) + distanceTraveled.get(index);
 	}
 }
