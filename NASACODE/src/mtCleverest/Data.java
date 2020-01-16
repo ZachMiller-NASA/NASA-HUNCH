2418c97a343894fcb816faab6831fb0803f15fff
Qubit7879 <57457413+Qubit7879@users.noreply.github.com>
2020-01-16 13:57:16 -0500

    Update Data.java

--- NASACODE/src/mtCleverest/Data.java
+++ NASACODE/src/mtCleverest/Data.java
@@ -10,6 +10,8 @@ public class Data {
 	public static ArrayList<Double> distanceTraveled = new ArrayList<Double>();
 	public static ArrayList<Double> height = new ArrayList<Double>();
 	public static ArrayList<Double> currentTime = new ArrayList<Double>();
+	public static ArrayList <Double> kineticEnergy = new ArrayList<Double>();
+	public static ArrayList <Double> potentialEnergy = new ArrayList<Double>();
 	
 	static ArrayList<Double> retrieve(Object request) {
 		if (request == "Altitude")
