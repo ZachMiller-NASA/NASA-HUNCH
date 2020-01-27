6f161f24e3a540ec8f0fcc91a46e89b6df11e903
SkylerGentner <57457186+SkylerGentner@users.noreply.github.com>
2020-01-27 13:19:00 -0500

    Added Kinetic and Potential Energy and Speed

--- NASACODE/src/mtCleverest/ProjectileMotion.java
+++ NASACODE/src/mtCleverest/ProjectileMotion.java
@@ -4,29 +4,42 @@ import java.util.ArrayList;
 
 public class ProjectileMotion {
 	
+	/**
+	 * No - Arg Constructor
+	 */
 	public ProjectileMotion() {
 		
 	}
 	
+	
 	/**
 	 * @param MOON_GRAVITY
+	 * @param MASS 
 	 * @param xVelocity
 	 * @param yVelocity
 	 * @param angleOfFall
 	 * @param distanceTraveled
 	 * @param height
+	 * @param potentialEnergy 
+	 * @param kineticEnergy 
+	 * @param speed 
 	 * @param index
 	 */
-	protected void allCalcs(final double MOON_GRAVITY, ArrayList<Double> xVelocity, 
+	protected void allCalcs(final double MOON_GRAVITY, final double MASS, ArrayList<Double> xVelocity, 
 			ArrayList<Double> yVelocity, ArrayList<Double> angleOfFall,
 			ArrayList<Double> distanceTraveled, ArrayList<Double> height, 
-			ArrayList<Double> time, int index) {
+			ArrayList<Double> time, ArrayList<Double> speed, ArrayList<Double> kineticEnergy,
+			ArrayList<Double> potentialEnergy, int index) {
+
 		yVelocity.add(yVelocity.get(index - 1) + MOON_GRAVITY * (time.get(index) - 
 				time.get(index - 1)));
-		xVelocity.add(xVelocity.get(index-1));
+		xVelocity.add(xVelocity.get(index - 1));
 		angleOfFall.add(calcAngle(xVelocity, yVelocity, index));
 		height.add(calcDistance(yVelocity, height, time, index));
 		distanceTraveled.add(calcDistance(xVelocity, distanceTraveled, time, index));
+		speed.add(calcSpeed(xVelocity, yVelocity, index));
+		kineticEnergy.add(calcKineticEnergy(MASS, speed, index));
+		potentialEnergy.add(calcPotentialEnergy(MOON_GRAVITY, MASS, height, index));
 	}
 
 	/**
@@ -44,8 +57,11 @@ public class ProjectileMotion {
 			ArrayList<Double> yVelocity, ArrayList<Double> angleOfFall,
 			ArrayList<Double> distanceTraveled, ArrayList<Double> height, ArrayList<Double> time,
 			int index, int timeIndex) {
-		xVelocity.add(xVelocity.get(index-1));
-		angleOfFall.add(Math.toDegrees(Math.atan((yVelocity.get(index + 1) / xVelocity.get(index)))));
+		
+		yVelocity.add(yVelocity.get(index - 1) + MOON_GRAVITY * (time.get(timeIndex) - 
+				time.get(index)));
+		xVelocity.add(xVelocity.get(index - 1));
+		angleOfFall.add(calcAngle(xVelocity, yVelocity, index));
 		height.add(calcDistance(yVelocity, height, time, index, timeIndex));
 		distanceTraveled.add(calcDistance(xVelocity, distanceTraveled, time, index, timeIndex));
 	}
@@ -78,12 +94,45 @@ public class ProjectileMotion {
 	 * @param distanceTraveled
 	 * @param time
 	 * @param index
-	 * @param finalIndex
+	 * @param timeIndex
 	 * @return THE DISTANCE FOR WHEN THE POD HITS THE GROUND
 	 */
-	protected double calcDistance(ArrayList<Double> velocity, ArrayList<Double> distanceTraveled,
-			ArrayList<Double> time, int index, int finalIndex) {
-		return (.5 * (velocity.get(finalIndex) + velocity.get(index)) * (time.get(finalIndex) - 
-				time.get(index))) + distanceTraveled.get(index);
+	protected Double calcDistance(ArrayList<Double> velocity, ArrayList<Double> distanceTraveled,
+			ArrayList<Double> time, int index, int timeIndex) {
+		return (.5 * (velocity.get(index) + velocity.get(index - 1)) * (time.get(timeIndex) - 
+				time.get(index))) + distanceTraveled.get(index - 1);
+	}
+	
+	/**
+	 * @param xVelocity
+	 * @param yVelocity
+	 * @param index
+	 * @return the vector of the x and y velocities as speed
+	 */
+	protected double calcSpeed(ArrayList<Double> xVelocity, ArrayList<Double> yVelocity, 
+			int index) {
+		return Math.sqrt(Math.pow(xVelocity.get(index), 2) + Math.pow(yVelocity.get(index), 2));
+	}
+	
+	/**
+	 * @param MASS
+	 * @param speed
+	 * @param index
+	 * @return kinetic energy in kilojoules
+	 */
+	protected double calcKineticEnergy(final double MASS, ArrayList<Double> speed, int index) {
+		return .5*MASS*Math.pow(speed.get(index), 2);
+	}
+	
+	/**
+	 * @param MOON_GRAVITY
+	 * @param MASS
+	 * @param height
+	 * @param index
+	 * @return potential energy in kilojoules
+	 */
+	protected double calcPotentialEnergy(final double MOON_GRAVITY, final double MASS,
+			ArrayList<Double> height, int index) {
+		return (MASS * -MOON_GRAVITY * height.get(index))/1000.0;
 	}
 }
