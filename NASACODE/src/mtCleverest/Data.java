a74566ff7968a563935e88134a1cd5abf95bd7d9
Qubit7879 <57457413+Qubit7879@users.noreply.github.com>
2020-02-10 12:14:53 -0500

    Update Data.java

--- NASACODE/src/mtCleverest/Data.java
+++ NASACODE/src/mtCleverest/Data.java
@@ -2,6 +2,10 @@ package mtCleverest;
 
 import java.util.ArrayList;
 
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+
 public class Data {
 	public static ArrayList<Double> xVelocity = new ArrayList<Double>();
 	public static ArrayList<Double> yVelocity = new ArrayList<Double>();
@@ -20,8 +24,12 @@ public class Data {
 			return currentTime;
 		else if (request == "Distance")
 			return distanceTraveled;
-		else if (request == "Speed") 
+		else if (request == "Velocity") 
 			return speed;
+		else if (request == "Potential Energy")
+			return potentialEnergy;
+		else if (request == "Kinetic Energy")
+			return kineticEnergy;
 		else
 			return angleOfFall;
 	}
